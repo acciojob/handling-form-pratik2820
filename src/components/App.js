8c62d5cd1219517687cbea0e95fa0db01d2786b5
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-09-25 10:02:16 +0000

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+
+  const[value,setValue]=useState("");
+
+  function updateValue(e){
+    setValue(e.target.value)
+  }
+
+  useEffect(()=>{
+    console.log(`value changed: ${value}`);
+  })
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" onChange={updateValue}/>
     </div>
   )
 }
